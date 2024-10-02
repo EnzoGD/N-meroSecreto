849012f38f1db08699a52339bd2a2ec9022d8f27
EnzoGD <106977951+EnzoGD@users.noreply.github.com>
2024-10-02 11:43:31 -0300

    upload

--- Número secreto/app.js
+++ Número secreto/app.js
@@ -1,3 +1,19 @@
 alert("Boas-vindas ao jogo do número secreto")
 let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 50.")
+let chute;
+
+while(chute != numeroSecreto){
+    chute = prompt("Escolha um número entre 1 e 10");
+
+if(chute == numeroSecreto){
+    console.log(`Isso aí!!! Você descobriu o número secreto ${numeroSecreto}`)
+} 
+else{
+    if(numeroSecreto> chute){
+        alert(`O número secreto é maior que ${chute}`)
+
+    } else {
+        alert(`O número secreto é menor que ${chute}`)
+    }
+}
+}

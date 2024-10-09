7f888530e4cf281b2313973f6ac33ace268f9ee1
EnzoGD <106977951+EnzoGD@users.noreply.github.com>
2024-10-09 11:51:06 -0300

    upload

--- Número secreto/app.js
+++ Número secreto/app.js
@@ -1,19 +1,21 @@
 alert("Boas-vindas ao jogo do número secreto")
 let numeroSecreto = 5;
 let chute;
+let tentativa =1;
 
+//enquanto = while
 while(chute != numeroSecreto){
-    chute = prompt("Escolha um número entre 1 e 10");
+        chute = prompt("Escolha um número entre 1 e 10");
 
-if(chute == numeroSecreto){
-    console.log(`Isso aí!!! Você descobriu o número secreto ${numeroSecreto}`)
+    if(chute == numeroSecreto){
+        alert(`Isso aí!!! Você descobriu o número secreto ${numeroSecreto} com ${tentativa} tentativas.`);
+    } 
+    else{
+        if(numeroSecreto> chute){
+            alert(`O número secreto é maior que ${chute}`);
+
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+    } tentativa++;
 } 
-else{
-    if(numeroSecreto> chute){
-        alert(`O número secreto é maior que ${chute}`)
-
-    } else {
-        alert(`O número secreto é menor que ${chute}`)
-    }
-}
-}
